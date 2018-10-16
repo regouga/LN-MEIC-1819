061c3ddaffbe7dcd4e6fb5d86720f8c7e1461a99
regouga <migasreg@me.com>
2018-10-16 19:16:14 +0100

    Final v2

--- Mini-Projeto 1/Solução/run.sh
+++ Mini-Projeto 1/Solução/run.sh
@@ -1,57 +1,72 @@
 cd ./a/1/
+chmod +x run.sh
 ./run.sh
 cd ..
 
 cd ./2/
+chmod +x run.sh
 ./run.sh
 cd ./Testes/
+chmod +x run.sh
 ./run.sh
 cd ..
 cd ..
 cd ..
 
 cd ./b/3/
+chmod +x run.sh
 ./run.sh
 cd ..
 
 cd ./4/
+chmod +x run.sh
 ./run.sh
 cd ./Testes/
+chmod +x run.sh
 ./run.sh
 cd ..
 cd ..
 cd ..
 
 cd ./c/5/
+chmod +x run.sh
 ./run.sh
 cd ..
 
 cd ./6/
+chmod +x run.sh
 ./run.sh
 cd ..
 
 cd ./7/
+chmod +x run.sh
 ./run.sh
 cd ..
 
 cd ./8/
+chmod +x run.sh
 ./run.sh
 cd ./Testes/
+chmod +x run.sh
 ./run.sh
 cd ..
 cd ..
 cd ..
 
 cd ./d/9/
+chmod +x run.sh
 ./run.sh
 cd ./Testes/
+chmod +x run.sh
 ./run.sh
 cd ..
 cd ..
 
 cd ./10/
+chmod +x run.sh
 ./run.sh
 cd ./Testes/
+chmod +x run.sh
 ./run.sh
 cd ..
 cd ..
