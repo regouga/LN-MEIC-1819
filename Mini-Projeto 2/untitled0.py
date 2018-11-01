c118b068fb80cefe8f79186a6636e2b82d610880
Miguel Regouga <migasreg@me.com>
2018-11-01 14:34:56 +0000

    Resultados iguais!

--- Mini-Projeto 2/untitled0.py
+++ Mini-Projeto 2/untitled0.py
@@ -447,17 +447,17 @@ train_y = list(training[:, 1])
 tf.reset_default_graph()
 # Build neural network
 net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 16)
-net = tflearn.fully_connected(net, 16)
+net = tflearn.fully_connected(net, 20)
+net = tflearn.fully_connected(net, 20)
 net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
 net = tflearn.regression(net)
 
 
 
 # Define model and setup tensorboard
-model = tflearn.DNN(net, tensorboard_dir='tflearn_logs', best_val_accuracy=0.9)
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 # Start training (apply gradient descent algorithm)
-model.fit(train_x, train_y, n_epoch=10000, show_metric=True)
+model.fit(train_x, train_y, n_epoch=1000, show_metric=True)
 model.save('model.tflearn')
 
 
